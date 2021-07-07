4b897dfaa1d3f52d5fd8ec4faea020735984e918
Yannick Severo <severoyannick@gmail.com>
2021-07-07 12:05:40 +0200

    Création graphique et statique

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,7 +3,19 @@ import './styles.scss';
 const App = () => {
   return (
     <div className="app">
-      App Component
+      <div className="widget">
+       <div className="widget__content">
+         <div className="widget__city">
+           Paris
+         </div>
+         <div className="widget__temp">
+           12°C
+         </div>
+       </div>
+       <div className="widget__img">
+         <img src="https://image.flaticon.com/icons/png/512/146/146199.png" />
+       </div>
+     </div>
     </div>
   );
 }
